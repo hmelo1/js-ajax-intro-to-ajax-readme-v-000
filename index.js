3861d36ae00ca0f44d505323543d6d2f3b57308a
Eddie Melo <heriberto@melo.nyc>
2018-05-08 23:09:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function showRepositories(event, data) {
 function getRepositories(){
   const req = new XMLHttpRequest();
   req.addEventListener("load", showRepositories);
-  req.open("GET", 'https://api.github.com/users/hmelo1/repos');
+  req.open("GET", 'https://api.github.com/users/octocat/repos');
   req.send();
 }
